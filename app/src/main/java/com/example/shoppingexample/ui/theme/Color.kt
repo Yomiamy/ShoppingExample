cbf9394e519e4781ebfb3336d2c6f494856923b9
randyh <randyh@cnct.co>
2025-04-25 00:12:53 +0800

    feat: 新增 色碼檔案

---
+++ app/src/main/java/com/example/shoppingexample/ui/theme/Color.kt
@@ -0,0 +1,10 @@
+package com.example.shoppingexample.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Color_008FDF = Color(0xFF008FDF)
+val Color_C25782 = Color(0xFFC25782)
+val Color_6200EE = Color(0xFF6200EE)
+val Color_FFFFFF = Color(0xFFFFFFFF)
+
+val Color_C0C0C0 = Color(0xFFC0C0C0)
