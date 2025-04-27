ec72c3cbea0a856a598b6f47ae339b191a742e29
randyh <randyh@cnct.co>
2025-04-27 21:41:57 +0800

    feat: 新增 取得商品列表API宣告

---
+++ app/src/main/java/com/example/shoppingexample/net/api/IApi.kt
@@ -0,0 +1,14 @@
+package com.example.shoppingexample.net.api
+
+import com.example.shoppingexample.model.ShoppingListInfo
+import retrofit2.Response
+import retrofit2.http.*
+
+interface IApi {
+
+    /**
+     * Get shopping list
+     */
+    @GET("/apis2/test/marttest.jsp")
+    suspend fun getShoppingList(): Response<ShoppingListInfo?>
+}
